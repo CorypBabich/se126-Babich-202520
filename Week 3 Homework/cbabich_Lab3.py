d51b0cdc5db76b062d52302e35443ff4981f6250
CorypBabich <cpbabich@email.NEIT.edu>
2025-01-24 17:11:53 -0500

    Finished Homework

--- Week 3 Homework/cbabich_Lab3.py
+++ Week 3 Homework/cbabich_Lab3.py
@@ -3,11 +3,14 @@
 #SE126.06
 #1-23-2025 [W1D2]
 
-#PROGRAM PROMPT:  
-
 #VARIABLE DICTIONARY
 
-
+#file       Holdes the read csv file as a 2D list
+#rec        Holds a record from file as a list, for each loop of the for loop
+#tooYoung   Holds the number of individuals that are too young to register
+#notReg     Holds the number of individuals that can register but have not
+#noVote     Holds the number of individuals that registered but didn't vote
+#didVote    Holds the number of individuals that voted. 
 
 #--------IMPORTS----------------------------------------------
 import csv
@@ -29,16 +32,17 @@ with open("voters_202040.csv") as csvfile:
 
         if( int(rec[1]) < 18 ):
             tooYoung += 1
-        elif(rec[2] == 'N'):
+        elif(rec[2].upper() == 'N'):
             notReg += 1
-        elif(rec[3] == 'N'):
+        elif(rec[3].upper() == 'N'):
             noVote += 1
         else:
             didVote += 1
 
-print(f"Too Young:{tooYoung:2} Didn't Register:{notReg:2}")
-print(f"Didn't Vote:{noVote:2} Did Vote: {didVote:2}")
-
+print(f"\nIndividuals not eligible to register:{tooYoung:2}")
+print(f"Individuals who did not Register:{notReg:2}")
+print(f"Didn't Vote:{noVote:2}")
+print(f"Did Vote:{didVote:2}")
 
 
 

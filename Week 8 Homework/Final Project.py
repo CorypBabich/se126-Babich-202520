b8a36753aee66f10d86c78ad61d914270817bab9
CorypBabich <cpbabich@email.NEIT.edu>
2025-03-03 11:13:15 -0500

    In Class Final work

--- Week 8 Homework/Final Project.py
+++ Week 8 Homework/Final Project.py
@@ -11,16 +11,24 @@ import csv
 import random
 
 #--FUNCTIONS-------------------------------------
+def updateEnemy(newEnem): #updates current foe states to match the stats of the enemy template it is passed to it.
+    currentFoe["name"] = newEnem["name"]
+    currentFoe["HP"] = newEnem["HP"]
+    currentFoe["atc"] = newEnem["atc"]
+    currentFoe["def"] = newEnem["def"]
+    currentFoe["ability"] = newEnem["ability"]
+
 def enAttack(): 
     return attack(currentFoe, player)
-def EnDefend():
+def enDefend():
     return defend(currentFoe)
 
 def attack(attacker, target):
-    defence = 1
+    damage = (attacker["atc"] - target["def"])
+
     if target["isDef"] == True:
-        defence = 2
-    damage = (attacker["atc"] - target["def"])/defence
+        damage = damage/2
+    
     
     # If the target's armor reduces the incoming damage to a negative number, it is set to 0.
     if damage < 0:
@@ -34,20 +42,34 @@ def attack(attacker, target):
 
     target["HP"] -= damage
 
+
 def defend(defender):
     defender["isDef"] = True
+
+    if defender == player:
+        print(f"Take evasive maneuvers!")
+    else:
+        print(f"They're trying to dodge our cannons")
 
 #--MAIN EXECUTING CODE----------------------------------
 
 # Player stats and enemy in focus
 player = {
     "name":"SS No Name",
+    "HPmax":10,
     "HP":10,
     "atc":5,
     "def":5,
     "isDef":False
 }
-currentFoe = {} # This is blank on compliation so that the enemy templates can fill it when they join the fight
+currentFoe = {
+    "name":"NoFoeShipError",
+    "HP":0,
+    "atc":0,
+    "def":0,
+    "isDef":False,
+    "ability":[]
+} # This is blank on compliation so that the enemy templates can fill it when they join the fight
                 # Funcations and other processes will only referrence the states of currentFoe
 
 # Enemy ship templates
@@ -56,24 +78,21 @@ sloop = {
     "HP":10,
     "atc":2,
     "def":0,
-    "isDef":False,
-    "ability":[enAttack,EnDefend]
+    "ability":[enAttack,enDefend]
 }
 battleShip = {
     "name":"Battleship",
     "HP":30,
     "atc":10,
     "def":2,
-    "isDef":False,
-    "ability":[enAttack,EnDefend]
+    "ability":[enAttack,enDefend]
 }
 capitalShip = {
     "name":"Capitalship",
     "HP":50,
     "atc":20,
     "def":4,
-    "isDef":False,
-    "ability":[enAttack,EnDefend]
+    "ability":[enAttack,enDefend]
 }
 
 # The order in which enemies attack the player
@@ -84,11 +103,12 @@ battleOrder = [
 ]
 
 contineuY = "Y"
+battleNum = 0
 
-currentFoe = sloop
+updateEnemy(battleOrder[battleNum]) #Sets the stats of currentFoe to the stats of the first enemy in the battle order list
 while contineuY == "Y":
     userInput = ""
-    player["isDef"] = False
+    player["isDef"] = False #if the player defended last turn, it is reset at the start of this turn
 
     print("\n")
     print(f"Enemy Ship: {currentFoe["name"]} | HitPoints :{currentFoe["HP"]:2} , Attack :{currentFoe["atc"]:2} , Defence :{currentFoe["def"]:2}")
@@ -102,5 +122,28 @@ while contineuY == "Y":
     elif userInput.upper() in "DEFENCE":
         player["isDef"] = True
 
+    currentFoe["isDef"] = False #if the enemy defended last turn, it is reset after the player's action but before it takes a turn.
+    
     currentFoe["ability"][random.randrange(len(currentFoe["ability"]))]()
+    '''
+                                                                       () "The rest of the line determines the function location, these parentheses"
+    currentfoe["ability"]                                                 "references the ability list, in the currentfoe dictionary"
+                         [                                            ]   "indicates the position of the ability list needed."
+                          random.randrange(                          )    "Returns a random number from 0 to the number given"
+                                           len(currentFoe["ability"])     "returns the length of the ability list"
+    '''
+
+    if currentFoe["HP"] <= 0:
+        print("\n!!! Ship Destroyed Captain !!!")
+        print("But a new enemy approaches")
+        input("\n(Press any button to continue)")
+
+        #Heal the player to full health
+        player["HP"] = player["HPmax"]
+
+        #increase the fight indicator by one and redefine the currentFoe accordingly
+        battleNum += 1
+        updateEnemy(battleOrder[battleNum])
+
+
 
